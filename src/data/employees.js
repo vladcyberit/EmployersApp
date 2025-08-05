c47d5b17b72e583c4af47c61a18c234bcc8b0c77
Vlad <vladcyberit@gmail.com>
2025-08-05 11:54:31 +0300

    added data/ folder and employees.js

---
+++ src/data/employees.js
@@ -0,0 +1,34 @@
+const employees = [
+    {
+        id: 1,
+        fullName: "John Peterson",
+        salary: 7900,
+        increase: false
+    },
+    {
+        id: 2,
+        fullName: "Michael Thomson",
+        salary: 8700,
+        increase: true
+    },
+    {
+        id: 3,
+        fullName: "David Miller",
+        salary: 7300,
+        increase: false
+    },
+    {
+        id: 4,
+        fullName: "James Wilson",
+        salary: 9400,
+        increase: true
+    },
+    {
+        id: 5,
+        fullName: "Robert Clark",
+        salary: 8100,
+        increase: false
+    }
+];
+
+export default employees;
