82484a617cfa070e234716a434032e8e39adf854
Vlad <vladcyberit@gmail.com>
2025-08-08 19:08:26 +0300

    turned functional component into a class component, implemented the functionality of awarding & liking an employee

--- src/components/employers-list-item/employers-list-item.jsx
+++ src/components/employers-list-item/employers-list-item.jsx
@@ -1,31 +1,58 @@
+import { Component } from "react";
 import "./employers-list-item.css";
 
-const EmployersListItem = ({fullName, salary, increase}) => {
-    const isAwarded = `list-group-item d-flex justify-content-between ${increase ? "increase" : ""}`;
+class EmployersListItem extends Component {    
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            like: false
+        }
+    }
 
-    return (
-        <li className={isAwarded}>
-            <span className="list-group-item-label">{fullName}</span>
-            <input 
-                type="text" 
-                className="list-group-item-input" 
-                defaultValue={salary + "$"}/>
-            <div className="d-flex justify-content-center align-items-center">
-                <button 
-                    type="button" 
-                    className="btn-trophy btn-sm">
-                    <i className="fa-solid fa-trophy"></i>
-                </button>
+    setIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }));
+    }
 
-                <button 
-                    type="button" 
-                    className="btn-trash btn-sm">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
+    setLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+        const { fullName, salary } = this.props;
+        const { increase, like } = this.state;
+        const isAwarded = `list-group-item d-flex justify-content-between${increase ? " increase" : ""}${like ? " like" : ""}`;
+
+        return (
+            <li className={isAwarded}>
+                <span className="list-group-item-label"
+                    onClick={this.setLike}>{fullName}</span>
+                <input 
+                    type="text" 
+                    className="list-group-item-input" 
+                    defaultValue={salary + "$"}/>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                        type="button" 
+                        className="btn-trophy btn-sm"
+                        onClick={this.setIncrease}>
+                        <i className="fa-solid fa-trophy"></i>
+                    </button>
+
+                    <button 
+                        type="button" 
+                        className="btn-trash btn-sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
 }
  
 export default EmployersListItem;
