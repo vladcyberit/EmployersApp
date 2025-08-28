d13e3c6093f95be1da02ccb32ca199115883e062
Vlad <vladcyberit@gmail.com>
2025-08-28 18:31:43 +0300

    realised a simple accessibility in list of employees

--- src/components/employers-list-item/employers-list-item.jsx
+++ src/components/employers-list-item/employers-list-item.jsx
@@ -5,26 +5,35 @@ const EmployersListItem = ({ fullName, jobTitle, salary, onDelete, onToggleStatu
 
     return (
         <li className={isAwarded}>
-            <span className="list-group-item-label"
+            <button
+                aria-label={`Set a status to employee: ${fullName} "likely to be promoted"`}
+                type="button"
+                className="list-group-item-label btn-name"
                 onClick={onToggleStatus}
-                data-toggle="like">{fullName}</span>
-            <span className="list-group-item-label">{jobTitle}</span>
+                data-toggle="like">{fullName}</button>
+            <span 
+                aria-label={`Job Title of an employee: ${jobTitle}`}
+                tabIndex={0}
+                className="list-group-item-label">{jobTitle}</span>
             <input 
+                aria-label={`Salary ${salary + "$"}`}
                 type="text" 
                 className="list-group-item-input" 
                 defaultValue={salary + "$"}/>
             <div className="d-flex justify-content-center align-items-center">
                 <button 
+                    aria-label={`Reward an employee: ${fullName}`}
                     type="button" 
-                    className="btn-trophy btn-sm"
+                    className="btn-trophy btn-sm btn-status"
                     onClick={onToggleStatus}
                     data-toggle="increase">
                     <i className="fa-solid fa-trophy"></i>
                 </button>
 
                 <button 
+                    aria-label={`Delete an employee: ${fullName}`}
                     type="button" 
-                    className="btn-trash btn-sm"
+                    className="btn-trash btn-sm btn-status"
                     onClick={onDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
